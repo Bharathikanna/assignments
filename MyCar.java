e9eb56132998018eaaaa7eeadd0cec9688bfe4f1
Bharathikanna <111111479+Bharathikanna@users.noreply.github.com>
2022-10-12 00:47:43 +0530

    Add files via upload

---
+++ MyCar.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class MyCar {
+	
+	static void main (String[] args) {
+		FourWheeler MyCar =new FourWheeler();
+		MyCar.applyAccelerate();
+		MyCar.applyBreak();
+		MyCar.applyGear();
+		MyCar.switchOnAc();
+	}
+
+}
---
+++ Mymobile.java
@@ -0,0 +1,16 @@
+package Mobile;
+
+import week1.day2.Mobile;
+
+public class Mymobile {
+	
+	public static void main (String[] args) {
+		
+		Mobile Mymobile= new Mobile();
+		Mymobile.shareDocument();
+		Mymobile.sendMessage();
+		Mymobile.call();
+		
+	}
+
+}
